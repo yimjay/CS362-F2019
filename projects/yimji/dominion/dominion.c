350798db244b75b9429e2a10b45d8701db7ffd09
JY <yimji@oregonstate.edu>
2019-10-12 11:24:04 -0600

    place mine into separate function

--- projects/yimji/dominion/dominion.c
+++ projects/yimji/dominion/dominion.c
@@ -806,7 +806,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
         return -1;
 
     case mine:
-        j = state->hand[currentPlayer][choice1];  //store card we will trash
+        mineCardEffect(card, choice1, choice2, choice3, state, handPos, bonus, currentPlayer, nextPlayer);
+        /*j = state->hand[currentPlayer][choice1];  //store card we will trash
 
         if (state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold)
         {
@@ -838,7 +839,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
             }
         }
 
-        return 0;
+        return 0;*/
 
     case remodel:
         j = state->hand[currentPlayer][choice1];  //store card we will trash
@@ -1262,6 +1263,44 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
     return -1;
 }
+
+int mineCardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus, int currentPlayer, int nextPlayer)
+{
+    j = state->hand[currentPlayer][choice1];  //store card we will trash
+
+    if (state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold)
+    {
+        return -1;
+    }
+
+    if (choice2 > treasure_map || choice2 < curse)
+    {
+        return -1;
+    }
+
+    if ( (getCost(state->hand[currentPlayer][choice1]) + 3) > getCost(choice2) )
+    {
+        return -1;
+    }
+
+    gainCard(choice2, state, 2, currentPlayer);
+
+    //discard card from hand
+    discardCard(handPos, currentPlayer, state, 0);
+
+    //discard trashed card
+    for (i = 0; i < state->handCount[currentPlayer]; i++)
+    {
+        if (state->hand[currentPlayer][i] == j)
+        {
+            discardCard(i, currentPlayer, state, 0);
+            break;
+        }
+    }
+
+    return 0;
+}
+
 
 int discardCard(int handPos, int currentPlayer, struct gameState *state, int trashFlag)
 {
--- projects/yimji/dominion/dominion_helpers.h
+++ projects/yimji/dominion/dominion_helpers.h
@@ -11,5 +11,9 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3,
                struct gameState *state, int handPos, int *bonus);
+int mineCardEffect(int card, int choice1, int choice2, int choice3,
+                   struct gameState *state, int handPos, int *bonus,
+                   int currentPlayer, int nextPlayer);
+
 
 #endif
