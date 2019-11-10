d5a23c23ccca7f94a63c7fba7c643797a1d45218
JY <yimjay@users.noreply.github.com>
2019-11-10 11:44:23 -0700

    added initial main function

--- projects/yimji/dominion/unittest3.c
+++ projects/yimji/dominion/unittest3.c
@@ -1,1 +1,17 @@
+/*
+** Name: Ji Soo Yim
+** CS 362 Software Engineering II
+** Assignment 3
+** Description: This unit test tests the refactored ambassador card
+*/
 
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <string.h>
+#include <stdio.h>
+
+int main(){
+
+    return 0;
+}
--- projects/yimji/dominion/unittest5.c
+++ projects/yimji/dominion/unittest5.c
@@ -1,1 +1,18 @@
 
+/*
+** Name: Ji Soo Yim
+** CS 362 Software Engineering II
+** Assignment 3
+** Description: This unit test tests the refactored mine card
+*/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <string.h>
+#include <stdio.h>
+
+int main(){
+    printf("This is the test for baron/n");
+    return 0;
+}
