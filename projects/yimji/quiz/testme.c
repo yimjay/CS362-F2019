e77bcbd15a6d883106848ad501ca266c3aa960da
JY <yimji@oregonstate.edu>
2019-11-10 21:19:32 -0700

    test

--- projects/yimji/quiz/testme.c
+++ projects/yimji/quiz/testme.c
@@ -8,7 +8,7 @@ char inputChar()
     //rand usage adapted from
     //https://www.codingunit.com/c-reference-stdlib-
     //h-function-rand-generate-a-random-number
-    char randomChar = (rand()%(122-97))+97;
+    char randomChar = (rand() % (116 - 101 + 1)) + 101;
     return randomChar;
 }
 
