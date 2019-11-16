d96717ef485ff91608de7bc58bb44d23271b3949
JY <yimjay@users.noreply.github.com>
2019-11-16 13:28:39 -0700

    test r1

--- projects/yimji/dominion/randomtestcard1.c
+++ projects/yimji/dominion/randomtestcard1.c
@@ -48,7 +48,7 @@ int main(){
         thisPlayer = inputInt(0, 1);
         choice1 = inputInt(0, 2);
 
-        if(thisPlayer == 0){
+        if(thisPlayer == 1){
             testG.hand[thisPlayer][numPlayers] = estate;
         }
         baronCardEffect(choice1, &testG, thisPlayer);
